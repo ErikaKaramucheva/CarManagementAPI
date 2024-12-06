2c3a060c6926aec1556abfb8fe1caa81ebbdd892
Erika Karamucheva <vesselinova11@gmail.com>
2024-12-06 15:20:08 +0200

    Initial commit

---
+++ src/main/java/uni/pu/fmi/CarManagementAPI/CarManagementApiApplication.java
@@ -0,0 +1,13 @@
+package uni.pu.fmi.CarManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApiApplication.class, args);
+	}
+
+}
